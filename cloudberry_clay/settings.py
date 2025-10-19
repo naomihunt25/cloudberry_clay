8e2e3be3cd14b175095f6827d521171864119da7
naomihunt25 <7tkz8agg@students.codeinstitute.net>
2025-10-19 17:37:31 +0100

    chore: Added hero image and more stylings to index

--- cloudberry_clay/settings.py
+++ cloudberry_clay/settings.py
@@ -127,11 +127,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  
+]
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"  
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
