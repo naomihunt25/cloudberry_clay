45737c94310d10d0ada41bfd703ae43b8d1795b8
naomihunt25 <7tkz8agg@students.codeinstitute.net>
2025-11-16 23:20:23 +0000

    fix: ALLOWED_HOSTS and disable DEBUG for Heroku

--- cloudberry_clay/settings.py
+++ cloudberry_clay/settings.py
@@ -10,12 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    'cloudberryclay-738fadfc0577.herokuapp.com/',
+    'cloudberryclay-738fadfc0577.herokuapp.com',
 ]
 
 
