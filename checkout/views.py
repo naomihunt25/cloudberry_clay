05d8aaa7c9da192782b98e47d338e99aa04bbf33
naomihunt25 <7tkz8agg@students.codeinstitute.net>
2025-11-16 11:05:35 +0000

    feat: Started to add stripe element payment function

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51SU3NE7oUblbUovpmqlhjYw27YQGRmbFhxnIYFZRjbEBQ19I0B0ieBGVOMOBrDbfLkoUGAAJ7ahaTUElmksTs8nC00GFXycZls',
+        'client_secret': 'test',
     }
 
     return render(request, template, context)
